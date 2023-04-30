e65e413bc82b801f392414c2ee5e331eaeba8775
Rup Raushan <r4rinku0101@gmail.com>
2023-04-30 14:33:18 +0530

    initial commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const mongooseURI = "mongodb+srv://rup:Rup%402907@aeronotes.riymjql.mongodb.net/aeronotes?retryWrites=true&w=majority"
-const mongooseURI = "mongodb://localhost:27017/aeronotes"
+const mongooseURI = "mongodb+srv://rup:Rup%402907@aeronotes.riymjql.mongodb.net/aeronotes?retryWrites=true&w=majority"
+// const mongooseURI = "mongodb://localhost:27017/aeronotes"
 
 // Function for connecting to database through mongoose
 const connectToMongo =()=>{
